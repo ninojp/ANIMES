dccf8b10446172051756283fe27bef6bea8ab677
Edenilson João Pereira <ninocriptocoin@gmail.com>
2023-02-02 18:27:44 -0300

    Criando as estruturas de pastas

---
+++ core/Config.php
@@ -0,0 +1,46 @@
+<?php
+namespace Core;
+if(!defined('$2y!10#OaHjLtR20hiD23TKNv(0$2)TkYur)$23$(zF')){ header("Location: https://localhost/animes/"); }
+
+/** Configurações básicas do site ANIME.
+ * @author NinoJP <meu.sem@gmail.com> - 01/02/2023 */
+
+abstract class Config
+{
+    /** ==========================================================================================
+     * Possui as constantes com as configurações.
+     * Configurações de endereço do projeto.
+     * Página principal do projeto.
+     * Credenciais de acesso ao banco de dados
+     * E-mail do administrador.
+     * @return void   */
+    protected function config(): void
+    {
+        //URL do projeto
+        define('URL', 'https://localhost/ANIMES/');
+        define('URLADM', 'https://localhost/ANIMES/adms/');
+        
+        //Página inicial (Principal)
+        define('CONTROLLER', 'Anime');
+        
+        //Página de erro (em caso de erro de carregamento)
+        define('CONTROLLERERRO', 'Erro');
+        
+        // E-mail definido como e-mail administrativo (depois alterar)
+        define('EMAILADM', 'meu.sem@gmail.com');
+
+        //Credenciais(Com senha) do banco de dados
+        define('HOST', 'localhost');
+        define('USER', 'NinoJP');
+        define('PASS', '3RtsEpuR21!@');
+        define('DBNAME', 'animes');
+        define('PORT', 3306);
+
+        //credenciais(Sem senha) do DB
+        // define('HOST','localhost');
+        // define('USER','root');
+        // define('PASS','');
+        // define('DBNAME','animes');
+        // define('PORT',3306);
+    }
+}
---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+session_start(); // Iniciar a sessão
+ob_start(); // Buffer de saida
+
+//Constante que define que o usuário está acessando páginas internas através da página "index.php".
+define('$2y!10#OaHjLtR20hiD23TKNv(0$2)TkYur)$23$(zF', true);
+
+//Carregar o Composer
+// require './vendor/autoload.php';
+
+//Instanciar a classe ConfigController, responsável em tratar a URL
+// $url = new Core\ConfigController();
+//Instanciar o método para carregar a página/controller
+// $url->loadPage();
+?>
+<h1>Começando uma nova jornada 01/02/2023</h1>
